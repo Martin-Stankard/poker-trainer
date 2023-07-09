bb911212abc5386d393eecfd3c56688c0262ed3b
Martin S <martin.stankard@hotmail.com>
2023-07-08 17:32:50 -0700

    some notes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("asdfasdf");
+//think eventually an express server here, running from docker compose up.
+
+//start with 5 decks, 
+
+//run through sorted counting check, 
+
+//run through shuffle counting check
+
+
+//Assume 5 
