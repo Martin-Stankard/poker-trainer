a54f274180b4c1153af7e4f078ffb89c6c6a4034
Martin S <martin.stankard@hotmail.com>
2023-07-08 17:48:08 -0700

    wiring in enums

---
+++ src/cards/Deck.js
@@ -0,0 +1,1 @@
+const { SUIT, VALUE } = require('./enums');
---
+++ src/cards/enums.js
@@ -0,0 +1,25 @@
+const SUIT = {
+    CLUBS: 'CLUBS',
+    DIAMONDS: 'DIAMONDS',
+    HEARTS: 'HEARTS',
+    SPADES: 'SPADES',
+};
+
+// Define the values enum
+ const VALUE = {
+    ACE: 'ACE',
+    TWO: 'TWO',
+    THREE: 'THREE',
+    FOUR: 'FOUR',
+    FIVE: 'FIVE',
+    SIX: 'SIX',
+    SEVEN: 'SEVEN',
+    EIGHT: 'EIGHT',
+    NINE: 'NINE',
+    TEN: 'TEN',
+    JACK: 'JACK',
+    QUEEN: 'QUEEN',
+    KING: 'KING',
+};
+
+module.exports = {SUIT, VALUE}
