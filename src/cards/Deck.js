b30b77e9071f9d9638100de9b36161ac49cf5b17
Martin S <martin.stankard@hotmail.com>
2023-07-08 17:52:30 -0700

    some things talking

--- src/cards/Deck.js
+++ src/cards/Deck.js
@@ -1,1 +1,5 @@
 const { SUIT, VALUE } = require('./enums');
+
+const testStringFromDeck = `suit is clubs:${SUIT.CLUBS}, value is four:${VALUE.FOUR}`
+
+module.exports = { testStringFromDeck }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //think eventually an express server here, running from docker compose up.
-
+const {testStringFromDeck} = require('./cards/Deck')
 //start with 5 decks, 
-
+console.log(testStringFromDeck);
 //run through sorted counting check, 
 
 //run through shuffle counting check
