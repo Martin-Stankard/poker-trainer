d1f92b8f50470bae35c5b6f9cc4c3376575d330d
Martin S <martin.stankard@hotmail.com>
2023-07-08 18:17:10 -0700

    have a deck

--- src/cards/Deck.js
+++ src/cards/Deck.js
@@ -1,5 +1,43 @@
 const { SUIT, VALUE } = require('./enums');
 
-const testStringFromDeck = `suit is clubs:${SUIT.CLUBS}, value is four:${VALUE.FOUR}`
 
-module.exports = { testStringFromDeck }
+function generateDeck(numberOfDecks, deckName) {
+
+    //if numberOfDecks is falsy, assume default is 5
+    if (!numberOfDecks) {
+        numberOfDecks = 5
+    }
+    const deck = {
+        deckNames: [],
+        cardCount: 0,
+        cards: [],
+        drawCard: function () {
+            if (this.cards.length === 0) {
+                console.log('No cards left in the deck');
+                return null;
+            }
+            this.cardCount--;
+            return this.cards.pop();
+        },
+    };
+
+    // Generate all possible unique cards
+    for (i = 1; i <= numberOfDecks; i++) {
+        deck.deckNames.push("" + i)
+        for (const suit in SUIT) {
+            for (const value in VALUE) {
+                const card = {
+                    suit: SUIT[suit],
+                    value: VALUE[value],
+                    hidden: "" + i,
+                };
+                deck.cards.push(card);
+                deck.cardCount++;
+            }
+        }
+
+        return deck;
+    }
+}
+
+module.exports = { generateDeck }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 //think eventually an express server here, running from docker compose up.
-const {testStringFromDeck} = require('./cards/Deck')
+const { generateDeck } = require('./cards/Deck')
 //start with 5 decks, 
-console.log(testStringFromDeck);
+// Usage example
+const myDeck = generateDeck();
+
+console.log(myDeck.cardCount); // Output: 52
+console.log(myDeck.deckNames);
+
+console.log(myDeck.drawCard())
+
 //run through sorted counting check, 
 
 //run through shuffle counting check
